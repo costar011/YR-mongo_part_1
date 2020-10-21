e4b37a6ce40f38d69a4caa60c19a306e84c980b1
YERIM <dnaer456@gmail.com>
2020-10-21 16:05:50 +0900

    server start

--- src/app.js
+++ src/app.js
@@ -5,13 +5,7 @@ dotenv.config();
 
 const app = express();
 
-connect();
 app.set(`PORT`, process.env.PORT);
-
-app.get("/", async (req, res) => {
-  const result = await Student.find({}, {});
-  res.send(result);
-});
 
 app.listen(app.get(`PORT`), () => {
   console.log(`Server Start ::`, app.get(`PORT`));
