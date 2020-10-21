466d27422b2f9c1a1f2b0cba1daa23babf6e5e27
YERIM <dnaer456@gmail.com>
2020-10-21 15:42:31 +0900

    error

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+connect();
+app.set(`PORT`, process.env.PORT);
+
+app.get("/", async (req, res) => {
+  const result = await Student.find({}, {});
+  res.send(result);
+});
+
+app.listen(app.get(`PORT`), () => {
+  console.log(`Server Start ::`, app.get(`PORT`));
+});
