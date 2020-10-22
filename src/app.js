f9362e2dd3e3f322cc443d014576d21ce6b705af
YERIM <dnaer456@gmail.com>
2020-10-22 16:11:53 +0900

    yarn dev

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.set(`PORT`, process.env.PORT);
 
+app.get("/", async (req, res) => {});
+
 app.listen(app.get(`PORT`), () => {
   console.log(`Server Start ::`, app.get(`PORT`));
 });
