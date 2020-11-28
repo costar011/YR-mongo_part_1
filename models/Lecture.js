f6b157907fe3ed46b808f32a427963a39edba26c
YERIM <dnaer456@gmail.com>
2020-11-28 20:53:01 +0900

    mongo

--- models/Lecture.js
+++ models/Lecture.js
@@ -1,8 +1,8 @@
-import monogoose from "mongoose";
+import monogoose from "mongoose"; // mongoose를 사용하기 위해 import함
 
-const Schema = monogoose.Schema;
+const Schema = monogoose.Schema; // const <--변하지 않는다는 의미  Schema <-- 객체는 대문자로 시작함
 
-// {} <-- model를 정의하는 제이슨, {}
+// Lecture 라는 상수에 스키마를 만듬
 const Lecture = new Schema(
   {
     name: {
@@ -31,3 +31,4 @@ const Lecture = new Schema(
 
 export default monogoose.model(`Lecture`, Lecture, `Lecture`);
 // Lecture 라는 이름으로 Lecture으로 사용한다 DB에 있는 Lecture를.
+// (`별칭`, 실제 객체 , `몽고DB안에 있는 진짜 별칭스키마는 뭔데?`)
